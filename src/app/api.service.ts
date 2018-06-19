9192765c4af3d4b55848b69db93ef1132fac0827
Pablo Barreyro <pabarreyro@gmail.com>
2018-06-19 09:37:04 -0700

    Add http.get() logic for Meetup API and custom backend

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -10,14 +10,18 @@ const httpOptions = {
   providedIn: 'root'
 })
 export class apiService {
-
+    USER_URL = 'https://pdxmeetups.herokuapp.com/api';
+    MEETUPS_URL = `https://api.meetup.com/find/upcoming_events?photo-host=public&topic_category=34&page=20&text=`;
     constructor(private http:HttpClient) {}
+    keyword : String = "ruby";
+    sigId: String = "231200293";
+    sig: String = "53a911c54a8d172e98264d9bb8ea3f2f88f99e70";
 
     makeCalls() {
         return forkJoin(
-            this.http.get(''), // Meetup API
-            this.http.get(''), // User API
-            this.http.get('') // GoogleMaps API
+            this.http.get(`${this.MEETUPS_URL}${this.keyword}&sig_id=${this.sigId}&sig=${this.sig}`), // Meetup API
+            this.http.get(`${this.USER_URL}/users`) // User API
+            // this.http.get('') // GoogleMaps API
         );
     }
 }
