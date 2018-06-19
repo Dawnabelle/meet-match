2e17406f5562927ccbb811e21ede7dd851b0d6df
Pablo Barreyro <pabarreyro@gmail.com>
2018-06-19 08:28:04 -0700

    Set up forkJoin() for chaining API calls

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -1,9 +1,21 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import {forkJoin} from 'rxjs';
 
-@Injectable({
-  providedIn: 'root'
-})
-export class ApiService {
+const httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+};
 
-  constructor() { }
+@Injectable()
+export class apiService {
+
+    constructor(private http:HttpClient) {}
+
+    makeCalls() {
+        return forkJoin(
+            this.http.get(''), // Meetup API
+            this.http.get('') // User API
+            this.http.get('') // GoogleMaps API
+        );
+    }
 }
