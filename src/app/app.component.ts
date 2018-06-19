3a69508273677f2bc28fb03ac8debd8dbca60734
Pablo Barreyro <pabarreyro@gmail.com>
2018-06-18 19:10:35 -0700

    Center map on Portland and add styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,11 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  title = 'app';
+  title: string = 'MeetMatch';
+  lat: number = 51.678418;
+  lng: number = 7.809007;
 }
