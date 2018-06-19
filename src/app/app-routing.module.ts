a45f012518dc0c7dd97c0ebf7995d1af5b7dbea5
Pablo Barreyro <pabarreyro@gmail.com>
2018-06-18 19:50:22 -0700

    Register routes and import router-outlet

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+
+import { FriendsComponent }   from './friends/friends.component';
+import { EventsComponent }      from './events/events.component';
+import { DetailsComponent }  from './details/details.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/events', pathMatch: 'full' },
+  { path: 'events', component: EventsComponent },
+  { path: 'details', component: DetailsComponent },
+  { path: 'friends', component: FriendsComponent }
+];
 
 @NgModule({
-  imports: [
-    CommonModule
-  ],
-  declarations: []
+  exports: [ RouterModule ],
+  imports: [ RouterModule.forRoot(routes) ]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
