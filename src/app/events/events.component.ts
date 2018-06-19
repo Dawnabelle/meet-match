2b9e55b6ea4836cbea5ba2606bca3e14f4b6f8c4
Pablo Barreyro <pabarreyro@gmail.com>
2018-06-19 16:50:39 -0700

    Add logic for parsing and reformatting event response data

--- src/app/events/events.component.ts
+++ src/app/events/events.component.ts
@@ -9,7 +9,8 @@ import { ApiService } from '../api.service';
 
 export class EventsComponent implements OnInit {
 
-private events: Array<any> = [];
+  private events = new Map();
+  private dates : Date[] = [];
   constructor(private apiService: ApiService) { }
 
   ngOnInit() {
@@ -18,13 +19,37 @@ private events: Array<any> = [];
 
   public getEvents() {
     this.apiService.getEvents().subscribe(res => {
-      console.log(res);
       for (let i = 0; i < res['events'].length; i++) {
         if (res['events'][i].venue != undefined){
-          this.events.push(res['events'][i]);
-          console.log(this.events);
+          let endTime: number = parseInt(res['events'][i].time) + parseInt(res['events'][i].duration);
+
+          res['events'][i].time = new Date (parseInt(res['events'][i].time));
+          res['events'][i].duration = new Date (endTime);
+          // res['events'][i].local_date = this.reformatDates(res['events'][i].local_date);
+
+          if (this.events[res['events'][i].local_date] !== null && this.events[res['events'][i].local_date] != undefined) {
+            if (!Array.isArray(this.events[res['events'][i].local_date])){
+              let list = this.events[res['events'][i].local_date];
+              this.events[res['events'][i].local_date] = [list];
+            }
+            this.events[res['events'][i].local_date].push(res['events'][i]);
+          } else {
+              this.events[res['events'][i].local_date] = res['events'][i];
+              this.dates.push(new Date(res['events'][i].local_date));
+          }
         }
       }
     });
+    this.dates.sort(function(date1,date2) {
+      return date1.getTime() - date2.getTime();
+    });
+    console.log(this.events);
+    console.log(this.dates);
   }
+
+  public reformatDates(date : Date) {
+      let formattedDate: Date = new Date(date);
+      return formattedDate.toDateString();
+  }
+
 }
