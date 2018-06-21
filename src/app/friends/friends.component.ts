d58d42db06163c3fea453139e0711c467ede3c8b
Pablo Barreyro <pabarreyro@gmail.com>
2018-06-20 17:43:44 -0700

    Inject UserService

--- src/app/friends/friends.component.ts
+++ src/app/friends/friends.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 
+import { ApiService } from '../api.service';
+import { UserService } from '../user.service';
+
 @Component({
   selector: 'app-friends',
   templateUrl: './friends.component.html',
-  styleUrls: ['./friends.component.scss']
+  styleUrls: ['./friends.component.scss'],
+  providers: [ApiService, UserService]
 })
 export class FriendsComponent implements OnInit {
+  private users: Array<Object>;
 
-  constructor() { }
+  constructor(private apiService: ApiService, private userService: UserService) { }
 
   ngOnInit() {
+    this.apiService.saveUsers();
+    this.users = this.userService.getUsers();
+    console.log(this.users);
   }
 
 }
